f5ceecac72773307ba197b017056621877b912f1
Andrew Davisson <akdavisson4@gmail.com>
2021-12-03 09:26:46 -0700

    find least and most common

--- day3.py
+++ day3.py
@@ -9,7 +9,26 @@ def get_formatted_inputs(file_name):
 
 
 # Function to find most and least common in list
+# Assumes list of 0's and 1's
+def least_most_common(values):
+    zero_count = 0
+    one_count = 0
+
+    for value in values:
+        if int(value) == 0:
+            zero_count += 1
+        elif int(value) == 1:
+            one_count += 1
+
+    return [0, 1] if zero_count < one_count else [1, 0]
+
 
 # Function to split all inputs into separate lists
+def split_into_lists(values):
+    lists = [[int(value)] for value in values[0]]
+    print(lists)
 
-print(str(get_formatted_inputs(file_name)))
+    return lists
+
+
+print(str(split_into_lists(get_formatted_inputs(file_name)))[0])
