94bc89bc02b3729c8bca0b95c20ef28bb9a17d9f
Andrew Davisson <akdavisson4@gmail.com>
2021-12-04 22:07:02 -0700

    another shot

--- day3.py
+++ day3.py
@@ -23,82 +23,25 @@ def least_most_common(values):
     return [0, 1] if zero_count < one_count else [1, 0]
 
 
+# Get column using index
+def get_column(values, index):
+    column = map(lambda x: x[index], values)
+
+    return list(column)
+
+
 # Return lists of least and most values
 def least_most_lists(values):
     lcl = [0] * len(values[0])
     mcl = [0] * len(values[0])
 
     for i in range(0, len(values[0])):
-        [lc, mc] = calculate_columns(values, i)
+        column = get_column(values, i)
+        [lc, mc] = least_most_common(column)
         lcl[i] = str(lc)
         mcl[i] = str(mc)
 
     return [lcl, mcl]
-
-
-# Get least and most values from each 'column'
-def calculate_columns(values, column):
-    temp_list = [values[0][column]]
-
-    for i in range(1, len(values)):
-        temp_list.append(values[i][column])
-
-    return least_most_common(temp_list)
-
-
-def o2_generator_rating(values, index):
-    if len(values) == 1:
-        return values
-
-    index_list = []
-
-    for value in values:
-        index_list.append(value[index])
-
-    [lc, mc] = least_most_common(index_list)
-
-    if lc == mc:
-        mc = 1
-
-    matches = []
-
-    # for value in values:
-    #     if value[index] == mc:
-    #         matches.append(value)
-    #     if len(matches) == 0 &
-
-    for i in range(0, len(values)):
-        if int(values[i][index]) == int(mc):
-            matches.append(value)
-        elif len(matches) == 0 and i == len(values) - 1:
-            return values[i]
-
-    return o2_generator_rating(matches, index + 1)
-
-
-def co2_scrubber_rating(values, index):
-    if len(values) == 1:
-        return values
-
-    index_list = []
-
-    for value in values:
-        index_list.append(value[index])
-
-    [lc, mc] = least_most_common(index_list)
-
-    if lc == mc:
-        lc = 0
-
-    matches = []
-
-    for i in range(0, len(values)):
-        if int(values[i][index]) == int(lc):
-            matches.append(value)
-        elif len(matches) == 0 and i == len(values) - 1:
-            return values[i]
-
-    return o2_generator_rating(matches, index + 1)
 
 
 # Convert list of 'binary' to decimal
@@ -108,26 +51,38 @@ def binary_list_to_decimal(b_list):
     return int(b_num, 2)
 
 
+def o2_generator_rating(values, index, common_values):
+    if (len(values) == 1):
+        return values
+
+    matches = []
+
+    for i in range(0, len(values)-1):
+        if values[i][index] == common_values[index]:
+            matches.append(values[i])
+
+    if len(matches) < 1:
+        return [values[len(values) - 1]]
+
+    return o2_generator_rating(matches, index + 1, common_values)
+
+
 def main(file_name):
     formatted_inputs = get_formatted_inputs(file_name)
     [lc_list, mc_list] = least_most_lists(formatted_inputs)
     [epsilon_rate, gamma_rate] = [binary_list_to_decimal(
         lc_list), binary_list_to_decimal(mc_list)]
-    o2_generator_rating_value = o2_generator_rating(
-        formatted_inputs, 0)
-    co2_scrubber_rating_value = co2_scrubber_rating(
-        formatted_inputs, 0)
-    life_support_rating = int(
-        o2_generator_rating_value[0], 2) * int(co2_scrubber_rating_value[0], 2)
+
+    o2 = o2_generator_rating(formatted_inputs, 0, mc_list)
+    co2 = o2_generator_rating(formatted_inputs, 0, lc_list)
+    life_support_rating = int(o2[0], 2) * int(co2[0], 2)
 
     print("Epsilon Rate: " + str(epsilon_rate))
     print("Gamma Rate: " + str(gamma_rate))
     print("Power Comsumption: " + str(epsilon_rate * gamma_rate))
 
-    print("O2 Generator Rating: " +
-          str(o2_generator_rating_value))
-    print("CO2 Scrubber Rating: " +
-          str(co2_scrubber_rating_value))
+    print("Oxygen Generator Rating: " + str(o2))
+    print("CO2 Scrubber Rating: " + str(co2))
     print("Life Support Rating: " + str(life_support_rating))
 
 
